151c0fbc7bea55485ea480b6edeedde05fd5d79b
Takayuki Muranushi <muranushi@gmail.com>
2016-01-31 22:40:14 +0900

    safe-pred.

--- exercise-8-1-safe-pred/src/Main.hs
+++ exercise-8-1-safe-pred/src/Main.hs
@@ -1,5 +1,20 @@
 module Main where
 
+import Control.Spoon (spoon)
+import Control.DeepSeq (NFData)
+
+predMay :: (Enum a, NFData a) => a -> Maybe a
+predMay = spoon . pred
+
+pred3 ::  (Enum a, NFData a) => a -> Maybe a
+pred3 = undefined
+
+predN ::  (Enum a, NFData a) => Int -> a -> Maybe a
+predN 0 x = Just x
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+  print $ pred3 True
+  print $ predN 256 'a'
+  print $ predN 32 'z'
