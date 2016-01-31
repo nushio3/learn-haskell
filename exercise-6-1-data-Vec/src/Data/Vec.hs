0ebff137ea81c57dd19983fa18a61053f1373a5b
Takayuki Muranushi <muranushi@gmail.com>
2016-01-31 14:57:03 +0900

    update the vectors.

--- exercise-6-1-data-Vec/src/Data/Vec.hs
+++ exercise-6-1-data-Vec/src/Data/Vec.hs
@@ -5,5 +5,5 @@ data Vec a = Vec a a a
 (・) :: Vec a -> Vec a -> a
 (Vec ax ay az) ・ (Vec bx by bz) = undefined
 
-(×) :: Vec a -> Vec a -> a
+(×) :: Vec a -> Vec a -> Vec a
 (Vec ax ay az) × (Vec bx by bz) = undefined
