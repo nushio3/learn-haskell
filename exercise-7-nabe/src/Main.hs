077ab548f1d4b793461908128865d8810c216a96
Takayuki Muranushi <muranushi@gmail.com>
2016-02-01 22:27:26 +0900

    add nabe program template.

--- exercise-7-nabe/src/Main.hs
+++ exercise-7-nabe/src/Main.hs
@@ -1,5 +1,29 @@
 module Main where
 
+import qualified Data.Map as M
+
+-- guzaiがnabeに入っていたらその具材の個数を１減らす、
+-- guzaiの個数がゼロになったらその項目をMapから消す、
+-- ように、関数eatを追記してください。
+eat :: String ->  M.Map String Int -> M.Map String Int
+eat guzai nabe = nabe
+
+party ::   M.Map String Int -> IO ()
+party nabe = do
+  putStrLn $ "Nabe: " ++ show nabe
+  order <- getLine
+  let newNabe = eat order nabe
+  if False -- ここで、鍋が空(null)かどうかを判定してください。
+    then putStrLn "The party is over!"
+    else party newNabe
+
+readRecipe :: IO (M.Map String Int)
+readRecipe = do
+  content <- readFile "recipe.txt"
+  -- content の内容を解釈して、おいしそうな鍋の中身を作ってください！
+  return $ M.fromList [("Kuuki",1)]
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  initialNabe <- readRecipe
+  party initialNabe
